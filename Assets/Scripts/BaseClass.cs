084c54367c4867b0f41b3644445546bb4b8d546f
Kablaev@gmail.com <jetadore404>
2020-01-31 00:34:08 +0700

    added basic UI and Scene

---
+++ Assets/Scripts/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseClass : MonoBehaviour
+{
+    string Name;
+    string Color;
+    GameObject Model;
+
+    public void SetName (string name) {
+        Name = name;
+    }
+
+    public void SetColor (string color) {
+        Color = color;
+    }
+
+    public void SetModel (GameObject model) {
+        Model = model;
+    }
+}
