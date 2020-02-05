3117186db91635e143407e41fc42e4864a9d7f88
Kablaev@gmail.com <jetadore404>
2020-02-05 22:00:22 +0700

    line render in development

---
+++ Assets/Scripts/Base/HoveringSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoveringSlider : MonoBehaviour
+{
+    public Slider AuraSlider;
+
+
+    private void Start() {
+
+    }
+    private void Update() {
+        Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
+        AuraSlider.transform.position = namePose;
+    }
+}
